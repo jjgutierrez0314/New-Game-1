3f668fba2dbe4dd2727a7fdc8c9b45eaa10e999e
tom0898 <tom0898@sbcglobal.net>
2020-03-08 01:18:57 -0800

    Updated left-right movement, collision works better
    - also removed some leftover gifs in sprite folder

--- Wander/Assets/Scripts/Player/PlayerController.cs
+++ Wander/Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,17 @@ using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
+    Rigidbody2D rb2D;
     public Animator animator;
 
-    public float moveSpeed = 6f;
-    public float sprintSpeed = 10f;
+    public float moveSpeed = 16f;
+    public float sprintSpeed = 24f;
     float xMove = 0f;
 
     public float jumpVelocity = 7f;
     public float fallMultiplier = 2f;
     public float lowJumpMultiplier = 2f;
+    Vector3 velocity = Vector3.zero;
 
     bool isGrounded;
     bool facingRight = true;
@@ -26,7 +27,7 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
 
         if (OnLandEvent == null)
             OnLandEvent = new UnityEvent();
@@ -49,7 +50,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.velocity = Vector2.up * jumpVelocity;
+            rb2D.velocity = Vector2.up * jumpVelocity;
             animator.SetBool("isJumping", true);
         }
     }
@@ -57,11 +58,11 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // Physics to control jump height depending on how long the Jump button is pressed
-        if (rb.velocity.y < 0)
-            rb.velocity += Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-            rb.velocity += Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
-        animator.SetFloat("yVelocity", rb.velocity.y);
+        if (rb2D.velocity.y < 0)
+            rb2D.velocity += Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        else if (rb2D.velocity.y > 0 && !Input.GetButton("Jump"))
+            rb2D.velocity += Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        animator.SetFloat("yVelocity", rb2D.velocity.y);
 
         // Checking if player is grounded or not
         bool wasGrounded = isGrounded;
@@ -78,9 +79,8 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // Left-right movement & increasing move speed with shift
-        Vector3 movement = new Vector3(xMove, 0, 0);
-        transform.Translate(movement * Time.deltaTime);
+        Vector3 targetVelocity = new Vector2(xMove * 25f * Time.fixedDeltaTime, rb2D.velocity.y);
+        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, targetVelocity, ref velocity, 0.05f);
     }
 
     public void onLanding()
