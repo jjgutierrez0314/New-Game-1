dadafddaf6d5873a31cf9658216050379185526f
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-04-04 15:47:30 -0700

    fixed elevator, press R to trigger it

--- Wander/Assets/Scripts/Level Scripts/ElevatorTrigger.cs
+++ Wander/Assets/Scripts/Level Scripts/ElevatorTrigger.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class ElevatorTrigger : MonoBehaviour
 {
     public Elevator platform;
+    private bool isColliding;
+    
     private void OnTriggerEnter2D(Collider2D other) {
-        // if(Input.GetKey(KeyCode.R)) {
+        isColliding = true;
+    }
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.R) && isColliding) {
             platform.nextPlatform();
-        // }
+        }
     }
     
+    private void OnTriggerExit2D(Collider2D other) {
+        isColliding = false;
+    }
 }
