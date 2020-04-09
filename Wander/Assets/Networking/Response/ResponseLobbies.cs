efb6d63c43b79e3a1aacb0750fd2e66aa1d3ab0c
gangzhaorige <45158992+gangzhaorige@users.noreply.github.com>
2020-04-08 18:15:48 -0700

    Added Request Lobbies and Response Lobbies with implemented UI

---
+++ Wander/Assets/Networking/Response/ResponseLobbies.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+using System;
+
+public class ResponseLobbiesEventArgs : ExtendedEventArgs {
+
+	public short status { get; set; }
+	public string json { get; set; }
+	public ResponseLobbiesEventArgs() {
+		event_id = Constants.SMSG_LOBBY;
+	}
+}
+
+public class ResponseLobbies : NetworkResponse {
+	
+	private short status;
+	private string json;
+
+	public ResponseLobbies() {
+
+	}
+	
+	public override void parse() {
+		status = DataReader.ReadShort(dataStream);
+		if (status == 0) {
+			json = DataReader.ReadString(dataStream);
+		}
+	}
+	
+	public override ExtendedEventArgs process() {
+		ResponseLobbiesEventArgs args = null;
+		if(status == 0){
+			args = new ResponseLobbiesEventArgs();
+			args.status = status;
+			args.json = json;
+		} 
+		return args;
+	}
+}
