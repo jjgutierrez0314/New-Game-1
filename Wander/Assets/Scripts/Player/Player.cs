c8d66f59bbc015ba810a011aa030152b580e1a02
tom0898 <tom0898@sbcglobal.net>
2020-04-16 15:39:10 -0700

    removed network stuff

--- Wander/Assets/Scripts/Player/Player.cs
+++ Wander/Assets/Scripts/Player/Player.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private int health;
-    private ConnectionManager cManager;
-    private MessageQueue msgQueue;
 
     void Awake()
     {
         health = 100;
-        cManager = gameObject.GetComponent<ConnectionManager>();
-        msgQueue = gameObject.GetComponent<MessageQueue>();
-        msgQueue.AddCallback(Constants.SMSG_PLAYERHIT, responsePlayerHit);
     }
 
     // Update is called once per frame
@@ -26,19 +21,5 @@ public class Player : MonoBehaviour
     {
         health -= damage;
         Debug.Log("health : " + health);
-        cManager.send(requestPlayerHit(health));
-    }
-
-    public RequestPlayerHit requestPlayerHit(int health)
-    {
-        RequestPlayerHit request = new RequestPlayerHit();
-        request.send(health);
-        return request;
-    }
-
-    public void responsePlayerHit(ExtendedEventArgs eventArgs)
-    {
-        ResponsePlayerHitEventArgs args = eventArgs as ResponsePlayerHitEventArgs;
-        Debug.Log("Player health: " + args.ToString());
     }
 }
