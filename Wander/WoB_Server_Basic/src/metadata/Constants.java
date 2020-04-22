2a7a7f089fc0d7359c866940b6b66426e7e569fd
gangzhaorige <gangzhaorige.li@gmail.com>
2020-04-21 23:43:43 -0700

    Server Updated

--- Wander/WoB_Server_Basic/src/metadata/Constants.java
+++ Wander/WoB_Server_Basic/src/metadata/Constants.java
@@ -12,8 +12,6 @@ public class Constants {
     public final static short SMSG_HEARTBEAT = 202;
     public final static short CMSG_PLAYERS = 103;
     public final static short SMSG_PLAYERS = 203;
-    public static final short CMSG_TEST = 104;
-    public static final short SMSG_TEST = 204;
     public final static short CMSG_REG = 105;
     public final static short SMSG_REG = 205;
     public final static short CMSG_MOVE = 106;
@@ -22,6 +20,8 @@ public class Constants {
     public final static short SMSG_SPAWN = 207;
     public final static short CMSG_LOBBY = 108;
     public final static short SMSG_LOBBY = 208;
+    public final static short CMSG_KEY = 109;
+    public final static short SMSG_KEY = 209;
     // Other
     public static final String CLIENT_VERSION = "1.00";
     public static final int TIMEOUT_SECONDS = 90;
