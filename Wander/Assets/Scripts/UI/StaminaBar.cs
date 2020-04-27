7bffa26ae045a48e123a32a40c2833f17c34135f
dangle730 <45413320+dangle730@users.noreply.github.com>
2020-04-26 19:43:38 -0700

    HP & Stamina Bar Implemented

---
+++ Wander/Assets/Scripts/UI/StaminaBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxStamina(int stamina)
+    {
+        slider.maxValue = stamina;
+        slider.value = stamina;
+    }
+
+    public void SetStamina(int stamina)
+    {
+        slider.value = stamina;
+    }
+
+    // public IEnumerator RegenStamina(int currentStamina, int maxStamina)
+    // {
+    //     int newStam = currentStamina;
+
+    //     yield return new WaitForSeconds(2);
+
+    //     while (currentStamina < maxStamina)
+    //     {
+    //         currentStamina += maxStamina / 400;
+    //         slider.value = currentStamina;
+    //         yield return regenTick;
+    //     }
+    // }
+}
