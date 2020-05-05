ed896411e0b597cecdc803f0fa6bd8a1a2b18721
gangzhaorige <gangzhaorige.li@gmail.com>
2020-05-04 23:14:22 -0700

    Final Merge

--- Wander/Assets/Scripts/Audio/FootstepTriggers/footstepTriggerCave.cs
+++ Wander/Assets/Scripts/Audio/FootstepTriggers/footstepTriggerCave.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class footstepTriggerCave : MonoBehaviour
 {
-private CharacterSFX_Warrior characterSFX_Warrior;
+    private CharacterSFX_Warrior characterSFX_Warrior;
 
     // Start is called before the first frame update
     void Start()
     {
-        characterSFX_Warrior = GameObject.FindObjectOfType<CharacterSFX_Warrior> ();
+        characterSFX_Warrior = GameObject.FindObjectOfType<CharacterSFX_Warrior>();
     }
     void OnTriggerEnter2D(Collider2D col)
     {
