51dc2323e1b3cd250959bd043001b287e0e6465e
John Joshua Gutierrez <jgutie11@mail.sfsu.edu>
2020-05-12 02:36:37 -0700

    another merge

---
+++ Wander/Assets/Boss_Idle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Idle : StateMachineBehaviour
+{
+    public float speed = 2.5f;
+    public float attackRange = 3f;
+    Transform player;
+    Rigidbody2D rb;
+    LookAtPlayer enemy;
+
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       player = GameObject.FindGameObjectWithTag("Player").transform;
+       rb = animator.GetComponent<Rigidbody2D>();
+       enemy = animator.GetComponent<LookAtPlayer>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       enemy.lookAtPlayer();
+       Vector2 target = new Vector2(player.position.x, rb.position.y);
+       Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+       rb.MovePosition(newPos);
+
+       if (Vector2.Distance(player.position, rb.position) <= attackRange) {
+           animator.SetTrigger("isAttacking");
+       }
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       animator.ResetTrigger("isAttacking");
+    }
+
+}
---
+++ Wander/Assets/Scripts/Enemy/LookAtPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public bool isFlipped = false;
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        if(other.tag == "Player") {
+            player = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D (Collider2D other) {
+        // if(other.tag == "Player") {
+        //     player = null;
+        // }
+    }
+    public void lookAtPlayer() {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.transform.position.x && isFlipped) {
+            Debug.Log("flip is working ONE");
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        } else if (transform.position.x > player.transform.position.x && !isFlipped) {
+            Debug.Log("flip is working TWO");
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+}
